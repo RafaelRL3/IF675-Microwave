aad2c3faceba3c360e8572bc26649f622b62cae5
RafaelRL3 <rafellabio2@gmail.com>
2021-04-24 00:24:11 -0300

    mudancas no cod dos timer 10 e 6, inicializando tb para o timer 10

--- Hdl/timer/timer_six.v
+++ Hdl/timer/timer_six.v
@@ -1,39 +1,47 @@
 module timer_six(input wire [3:0] data, input wire loadn,input wire clk,input wire clrn, input wire en,
                output reg [3:0]  out, output reg tc, output reg zero );
 
-always @(posedge clk, posedge clrn)
+
+
+always @(clrn)
+begin
+    if(~clrn)
     begin
-        if(~en);
-        else if(clrn)
+        out = 0;
+        tc = 0; 
+        zero = 1;
+    end
+end
+
+
+always @(posedge clk)
+    begin
+        if(en)
         begin
-            out <= 4'b0000;
-            tc <= 1'b0; 
-            zero <= 1'b1;
-        end
-        else
-        begin
-            if(data==4'b0000)
+            if(~loadn)
             begin
-                out <= 4'b0101;
-                tc <= 1'b1;
-                zero <= 1'b0;
+                out <= data;
             end
-            else if(data==4'b0001)
+            else 
             begin
-                out <= 4'b0000;
-                tc <= 1'b0;
-                zero <= 1'b1;
-            end
-            else
+                if(out==0)
                 begin
-                    out <=  (data==4'b0101)? 4'b0100:
-                            (data==4'b0100)? 4'b0011:
-                            (data==4'b0011)? 4'b0010:4'b0001;
-                    tc <= 1'b0;
-                    zero <= 1'b0;
-                     
+                    out <= 5;
+                    tc <= 1;
+                    zero <= 0;
                 end
+                else if(out==1)
+                begin
+                    out <= out-1;
+                    tc <= 0;
+                    zero <= 1;
+                end
+                else
+                begin
+                    out <= out-1;
+                    tc <= 0;
+                    zero <= 0;
+                end
+            end
         end
     end
-
-endmodule
